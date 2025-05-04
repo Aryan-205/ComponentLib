6aafb8a07e294a599c706cb05b0f5bce78bbd24f
Aryan Bola <aaryann5002@gmail.com>
2025-05-04 23:49:45 +0530

    working on a new design

---
+++ 3DPackagingEffect/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+
+  return (
+    <>
+      <div className="bg-black h-screen flex justify-center items-center p-2">
+        <div className='flex p-2 '>
+          <div className="flex justify-center items-center text-white p-12 shadow-[inset_1px_10px_10px_1px_white]">
+            hello
+          </div>
+          <div className="flex justify-center items-center text-white p-2">
+            boy
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
