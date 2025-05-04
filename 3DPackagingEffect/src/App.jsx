70653a184821496c59178f397a68e57227373753
Aryan Bola <aaryann5002@gmail.com>
2025-05-05 00:12:12 +0530

    completed the design trial

--- 3DPackagingEffect/src/App.jsx
+++ 3DPackagingEffect/src/App.jsx
@@ -2,13 +2,22 @@ function App() {
 
   return (
     <>
-      <div className="bg-black h-screen flex justify-center items-center p-2">
-        <div className='flex p-2 '>
-          <div className="flex justify-center items-center text-white p-12 shadow-[inset_1px_10px_10px_1px_white]">
+      <div className="bg-gray-900 h-screen flex justify-center items-center p-12 gap-8 shadow-[inset_8px_8px_16px_1px_rgb(255,255,255,0.2),inset_-8px_-8px_16px_1px_rgb(0,0,0,0.5)] rounded-lg">
+        <div className='grid grid-cols-3 p-12 gap-8 shadow-[inset_8px_8px_16px_1px_rgb(255,255,255,0.2),inset_-8px_-8px_16px_1px_rgb(0,0,0,0.5)] rounded-lg'>
+          <div className="flex col-span-1 justify-center items-center text-white p-12 shadow-[inset_8px_8px_16px_1px_rgb(255,255,255,0.2),inset_-8px_-8px_16px_1px_rgb(0,0,0,0.5)] rounded-lg">
             hello
           </div>
-          <div className="flex justify-center items-center text-white p-2">
+          <div className="flex col-span-2 justify-center items-center text-white p-12 shadow-[inset_8px_8px_16px_1px_rgb(255,255,255,0.2),inset_-8px_-8px_16px_1px_rgb(0,0,0,0.5)] rounded-lg">
             boy
+          </div>
+          <div className="flex col-span-2 justify-center items-center text-white p-12 shadow-[inset_8px_8px_16px_1px_rgb(255,255,255,0.2),inset_-8px_-8px_16px_1px_rgb(0,0,0,0.5)] rounded-lg">
+            hello
+          </div>
+          <div className="flex justify-center items-center text-white p-12 shadow-[inset_8px_8px_16px_1px_rgb(255,255,255,0.2),inset_-8px_-8px_16px_1px_rgb(0,0,0,0.5)] rounded-lg">
+            boy
+          </div>
+          <div className="flex col-span-full justify-center items-center text-white p-12 shadow-[inset_8px_8px_16px_1px_rgb(255,255,255,0.2),inset_-8px_-8px_16px_1px_rgb(0,0,0,0.5)] rounded-lg">
+            dello
           </div>
         </div>
       </div>
